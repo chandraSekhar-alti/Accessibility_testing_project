edee3fbc5669de4ca8bd7c2b37c09d5949c7c359
chandraSekhar-alti <althichandu123@gmail.com>
2024-04-03 12:17:49 +0530

    added report to the execsting code

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -1,34 +1,48 @@
-import { test, expect } from "@playwright/test";
+import { test } from "@playwright/test";
 import AxeBuilder from "@axe-core/playwright";
-import { writeFileSync } from 'fs';
+import { generateAccessibilityReport } from "../utils.js";
 
-test('Accessibility Testing', async ({ page }) => {
+const directoryPath = "test-results";
+
+test("Validate FlipCart login page accessibility rules as per WCAG", async ({
+  page,
+}) => {
+  await page.goto("https://www.flipkart.com/");
+  const axeBuilder = await new AxeBuilder({ page }).analyze();
+  const reportFileName = "flipcartHomePageReport";
+  generateAccessibilityReport(axeBuilder, reportFileName, directoryPath);
+});
+
+test("Validate Myntra login page accessibility rules as per WCAG", async ({
+  page,
+}) => {
+  await page.goto("https://www.myntra.com/");
+  const axeBuilder = await new AxeBuilder({ page })
+    .disableRules([
+      "color-contrast",
+      "heading-order",
+      "image-alt",
+      "link-in-text-block",
+      "link-name",
+      "list",
+      "meta-viewport",
+      "region",
+    ])
+    .analyze();
+  const reportFileName = "myntraHomePageReport";
+  generateAccessibilityReport(axeBuilder, reportFileName, directoryPath);
+});
+
+test("Validate Amazon login page accessibility rules as per WCAG", async ({
+  page,
+}) => {
   await page.goto("https://www.amazon.com");
-  await page.waitForTimeout(5000);
 
   try {
-    // Run Axe analysis and store results
-    const results = await new AxeBuilder({ page }).analyze();
-
-    // Generate HTML report
-    const reportHtml = AxeBuilder.createReport(results);
-
-    // Write report to file
-    writeFileSync('accessibility_report.html', reportHtml, 'utf-8');
-    
-    console.log('Accessibility report generated: accessibility_report.html');
-
-    // Attach results to the test report
-    await test.attach('accessibilityScanResults', {
-      name: 'Accessibility Scan Results',
-      contentType: 'text/html',
-      body: reportHtml,
-    });
-
-    // Assert no violations found
-    expect(results.violations).toHaveLength(0);
-  } catch (e) {
-    // Handle any errors
-    console.error('Error occurred during accessibility testing:', e);
+    const axeBuilder = await new AxeBuilder({ page }).analyze();
+    const reportFileName = "amazonHomepageReport";
+    generateAccessibilityReport(axeBuilder, reportFileName, directoryPath);
+  } catch (error) {
+    console.error("Error occurred during accessibility testing:", error);
   }
 });
