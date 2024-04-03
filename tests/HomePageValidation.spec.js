e921879fd2f51257140049e5ebc9077c555b483d
chandraSekhar-alti <althichandu123@gmail.com>
2024-04-03 14:47:48 +0530

    added comments for script file

--- tests/HomePageValidation.spec.js
+++ tests/HomePageValidation.spec.js
@@ -1,48 +1,55 @@
-import { test } from "@playwright/test";
-import AxeBuilder from "@axe-core/playwright";
-import { generateAccessibilityReport } from "../utils.js";
+import { test } from '@playwright/test'
+import AxeBuilder from '@axe-core/playwright'
+import { generateAccessibilityReport } from '../utils.js'
 
-const directoryPath = "test-results";
+const directoryPath = 'test-results' // Defining the directory path where accessibility reports will be stored
 
-test("Validate FlipCart login page accessibility rules as per WCAG", async ({
-  page,
+test('Validate FlipCart login page accessibility rules as per WCAG', async ({
+  page
 }) => {
-  await page.goto("https://www.flipkart.com/");
-  const axeBuilder = await new AxeBuilder({ page }).analyze();
-  const reportFileName = "flipcartHomePageReport";
-  generateAccessibilityReport(axeBuilder, reportFileName, directoryPath);
-});
+  //Launching the web page
+  await page.goto('https://www.flipkart.com/')
+  // Perform accessibility analysis on the current page using AxeCore, storing the results in axeBuilder.
+  const axeBuilder = await new AxeBuilder({ page }).analyze()
+  const reportFileName = 'flipcartHomePageReport' // Defining the filename for the FlipCart homepage accessibility report
+  // Generating the accessibility report for the FlipCart homepage
+  generateAccessibilityReport(axeBuilder, reportFileName, directoryPath)
+})
 
-test("Validate Myntra login page accessibility rules as per WCAG", async ({
-  page,
+test('Validate Myntra login page accessibility rules as per WCAG', async ({
+  page
 }) => {
-  await page.goto("https://www.myntra.com/");
+  await page.goto('https://www.myntra.com/')
+  // Perform accessibility analysis on the current page using AxeCore, storing the results in axeBuilder.
   const axeBuilder = await new AxeBuilder({ page })
     .disableRules([
-      "color-contrast",
-      "heading-order",
-      "image-alt",
-      "link-in-text-block",
-      "link-name",
-      "list",
-      "meta-viewport",
-      "region",
+      // Disabling specific accessibility rules for Myntra homepage analysis
+      'color-contrast',
+      'heading-order',
+      'image-alt',
+      'link-in-text-block',
+      'link-name',
+      'list',
+      'meta-viewport',
+      'region'
     ])
-    .analyze();
-  const reportFileName = "myntraHomePageReport";
-  generateAccessibilityReport(axeBuilder, reportFileName, directoryPath);
-});
+    .analyze()
+  const reportFileName = 'myntraHomePageReport' // Defining the filename for the Myntra homepage accessibility report
+  // Generating the accessibility report for the Myntra homepage
+  generateAccessibilityReport(axeBuilder, reportFileName, directoryPath)
+})
 
-test("Validate Amazon login page accessibility rules as per WCAG", async ({
-  page,
+test('Validate Amazon login page accessibility rules as per WCAG', async ({
+  page
 }) => {
-  await page.goto("https://www.amazon.com");
-
+  await page.goto('https://www.amazon.com')
   try {
-    const axeBuilder = await new AxeBuilder({ page }).analyze();
-    const reportFileName = "amazonHomepageReport";
-    generateAccessibilityReport(axeBuilder, reportFileName, directoryPath);
+    // Perform accessibility analysis on the current page using AxeCore, storing the results in axeBuilder.
+    const axeBuilder = await new AxeBuilder({ page }).analyze()
+    const reportFileName = 'amazonHomepageReport' // Defining the filename for the Amazon homepage accessibility report
+    // Generating the accessibility report for the Amazon homepage
+    generateAccessibilityReport(axeBuilder, reportFileName, directoryPath)
   } catch (error) {
-    console.error("Error occurred during accessibility testing:", error);
+    console.error('Error occurred during accessibility testing:', error)
   }
-});
+})
